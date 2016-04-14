a2de3a30477877900fc59a13de54026b8e635ee6
Strashimir Rashev <sirius.rashev@gmail.com>
2016-04-14 14:22:10 +0300

    3 cards and card animation added todo: fix the cards layout when opening the menu

--- js/menu_icon_animation.js
+++ js/menu_icon_animation.js
@@ -3,13 +3,10 @@ function changeClass() {
         document.getElementById("nav-icon1").className =
             document.getElementById("nav-icon1").className.replace
             (/(?:^|\s)open(?!\S)/g, '')
-        document.getElementById("main_content").style.width = "1366px";
         document.getElementById("main_content").style.marginLeft = "0px";
     }
     else {
         document.getElementById("nav-icon1").className += " open";
-        document.getElementById("main_content").style.width = "1126px";
-        document.getElementById("main_content").style.marginLeft = "240px";
-
+        document.getElementById("main_content").style.marginLeft = "150px";
     }
 }
