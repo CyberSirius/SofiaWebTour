c70abab4c44594fdb352fdf8a1fd21ce78d29f07
Teodora <tedi_vasielva1@abv.bg>
2016-04-17 19:35:16 +0300

    some shittttt

--- js/menu_icon_animation.js
+++ js/menu_icon_animation.js
@@ -1,12 +1,45 @@
 function changeClass() {
+	
+	var getClassOf = Function.prototype.call.bind(Object.prototype.toString);
+	
     if (document.getElementById("nav-icon1").className.match(/(?:^|\s)open(?!\S)/)) {
         document.getElementById("nav-icon1").className =
             document.getElementById("nav-icon1").className.replace
             (/(?:^|\s)open(?!\S)/g, '')
-        document.getElementById("main_content").style.marginLeft = "0px";
+        document.getElementById("main_container").style.marginLeft = "0px";
+        var cards = document.getElementsByClassName("card__container");
+        for (i = 0; i < cards.length; i++) { 
+       	 if(!hasClass(cards[i], "card__container--closed")){
+            	cards[i].style.marginLeft = "0px";
+            }
+       }
     }
     else {
         document.getElementById("nav-icon1").className += " open";
-        document.getElementById("main_content").style.marginLeft = "250px";
+        var container = document.getElementById("main_container");
+        container.style.marginLeft = "250px";
+        var obf = document.getElementsByClassName("mdl-layout__obfuscator");
+        var layout = document.getElementById("layout");
+        obf[0].style.height = layout.offsetHeight;
+        var cards = document.getElementsByClassName("card__container");
+        for (i = 0; i < cards.length; i++) { 
+        	 if(!hasClass(cards[i], "card__container--closed")){
+             	cards[i].style.marginLeft = "150px";
+             }
+        }
+       
     }
 }
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+function addMethodToObf() {
+	
+	var obf = document.getElementsByClassName("mdl-layout__obfuscator");
+	obf[0].addEventListener("click", changeClass);
+}
+
+
+
